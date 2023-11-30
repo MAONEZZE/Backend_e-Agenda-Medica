964cd6fa76ede6688cc0c315204e8636dc659c6f
Ruan Sanchez <103606152+MAONEZZE@users.noreply.github.com>
2023-11-30 13:17:43 -0300

    atualização no endpoint de autenticação

--- Backend/eAgendaMedica.Api/Controllers/ModuloAutenticacao/AutenticacaoController.cs
+++ Backend/eAgendaMedica.Api/Controllers/ModuloAutenticacao/AutenticacaoController.cs
@@ -86,7 +86,7 @@ namespace eAgendaMedica.Api.Controllers.ModuloAutenticacao
         }
 
         // JWT - jason web token
-        public static TokenViewModel GerarJwt(Usuario usuario, DateTime dataExpiracao)//Monta o view model
+        public static TokenViewModel GerarJwt(Usuario usuario, DateTime dataExpiracao) //Monta o view model
         {
             string token = CriarChaveToken(usuario, dataExpiracao);
 
@@ -106,11 +106,11 @@ namespace eAgendaMedica.Api.Controllers.ModuloAutenticacao
         {
             var tokenHandler = new JwtSecurityTokenHandler();//manipulador de tokens
 
-            var segredo = Encoding.ASCII.GetBytes("SegredoeAgendaMedica");//chave da criptografia
+            var segredo = Encoding.ASCII.GetBytes("SegredoeAgendaMedica");//chave da criptografia está sendo convertida em bytes
 
-            var algoritmo = SecurityAlgorithms.HmacSha256Signature;
+            var algoritmo = SecurityAlgorithms.HmacSha256Signature;// define o algoritmo de criptografia
 
-            var chaveSimetrica = new SymmetricSecurityKey(segredo);// chave compartilhada entre o emissor (quem assina) e o receptor (quem verifica a assinatura)
+            var chaveSimetrica = new SymmetricSecurityKey(segredo);// chave compartilhada entre o emissor (quem assina) e o receptor (quem verifica a assinatura), com o segredo
 
             var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
             {
@@ -122,12 +122,12 @@ namespace eAgendaMedica.Api.Controllers.ModuloAutenticacao
 
                 Expires = dataExpiracao,
 
-                SigningCredentials = new SigningCredentials(chaveSimetrica, algoritmo)//algoritmo da criptografia
+                SigningCredentials = new SigningCredentials(chaveSimetrica, algoritmo) //algoritmo da criptografia
 
                 //SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(segredo), SecurityAlgorithms.HmacSha256Signature)
             });
 
-            string chaveToken = tokenHandler.WriteToken(token);
+            string chaveToken = tokenHandler.WriteToken(token); // converte o token em uma string jwt
 
             return chaveToken;
         }
